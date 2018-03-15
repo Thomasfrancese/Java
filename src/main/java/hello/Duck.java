931c27f7b109c4a73c9a7fc62f44bb5cbf7fa326
Thomas <thomas.francese@campus-numerique-in-the-alps.com>
2018-03-15 17:03:30 +0100

    add coin coin

---
+++ src/main/java/hello/Duck.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Duck extends Animal {
+
+    public String sayHello()
+
+    {
+        return "Coin! coin!";
+    }
+}
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,9 +4,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         Bonjour human = new Human();
         Bonjour dog = new Dog();
+        Bonjour duck = new Duck();
 
         System.out.println(human.sayHello());
         System.out.println(dog.sayHello());
+        System.out.println(duck.sayHello());
 
     }
 }
